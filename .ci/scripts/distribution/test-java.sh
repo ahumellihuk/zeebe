cd93c09e1a2ac91e1ba53ae13c461ad0ddcf30b1
pihme <pihme@users.noreply.github.com>
2020-05-25 17:58:57 +0200

    fixup

--- .ci/scripts/distribution/test-java.sh
+++ .ci/scripts/distribution/test-java.sh
@@ -11,7 +11,7 @@ if [[ $status != 0 ]]; then
   exit $status;
 fi
 
-if grep -q "\[INFO\]  Build failures were ignored\." test.txt; then
+if grep -q "\[INFO\] Build failures were ignored\." test.txt; then
   rm test.txt
   exit 1
 fi
